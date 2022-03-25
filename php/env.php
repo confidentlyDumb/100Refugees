019e6c622a748ad5e81429a1d893e0104eb09946
confidentlyDumb <alexandrabushure@gmail.com>
2022-03-25 12:58:02 +0100

    display_errors(0)

--- php/env.php
+++ php/env.php
@@ -4,7 +4,7 @@ define('DB_SERV',                 'localhost');
 define('DB_USER',                 '');
 define('DB_PASS',                 '');
 define('DB_DB',                   '');
-ini_set('display_errors',         1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors',         0);
+ini_set('display_startup_errors', 0);
 
 ?>
